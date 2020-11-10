5b46413b948d00b5c84d85401225f752d8d4c5df
Oluwaseyi Aimudo <oluwaseyiaimudo@Oluwaseyis-MacBook-Pro.local>
2020-11-10 18:20:44 +0100

    Made it responsive

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('yo')
+console.log(window.innerWidth)
+new Glider(document.querySelector('.glider'),{
+    slidesToShow: 5,
+    draggable: true,
+    dots: '#dots',
+    arrows: {
+        prev: '.glider-prev',
+        next: '.glider-next'
+    }
+});
+
+const email = document.getElementById('email')
+const subscribeBtn = document.querySelector('.subscribe-btn')
+
+subscribeBtn.addEventListener('click', (e) => {
+    let details = []
+
+    details.push(email.value)
+
+    console.log(details)
+    e.preventDefault()
+})
