8e4bb6ac0bcf0ed167a6f9e17bf3ac523104e52e
Oluwaseyi Aimudo <oluwaseyiaimudo@Oluwaseyis-MacBook-Pro.local>
2020-11-14 20:02:06 +0100

    mobile nav

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-console.log(window.innerWidth)
+const hamburger = document.querySelector('.hamburger')
+const navUL = document.querySelector('.nav-ul')
+hamburger.addEventListener('click', () => {
+    // navUL.style.display = 'block'
+    // navUL.classList.add('show')
+    navUL.classList.toggle('check')
+})
+
+let links = document.querySelector('.nav-link')
+const linkOne = document.querySelector('.nav-link-one')
+const linkTwo = document.querySelector('.nav-link-two')
+linkTwo.addEventListener('click', () => {
+    navUL.classList.remove('check')
+})
+linkOne.addEventListener('click', () => {
+    navUL.classList.remove('check')
+})
+links.addEventListener('click', ()=>{
+        navUL.classList.remove('check')
+    // console.log('clicked')
+})
+
+
+
 new Glider(document.querySelector('.glider'),{
     slidesToShow: 5,
     draggable: true,
@@ -8,6 +31,8 @@ new Glider(document.querySelector('.glider'),{
         next: '.glider-next'
     }
 });
+
+
 
 const email = document.getElementById('email')
 const subscribeBtn = document.querySelector('.subscribe-btn')
@@ -20,3 +45,5 @@ subscribeBtn.addEventListener('click', (e) => {
     console.log(details)
     e.preventDefault()
 })
+
+console.log(window.innerWidth)
